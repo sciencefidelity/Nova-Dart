bd2fd02a17c266f30d6986eb645cc5fd0c2b5b47
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-04 19:58:13 +0000

    Add main packages

---
+++ rollup.config.main.js
@@ -0,0 +1,13 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/main.ts",
+  plugins: [typescript(), commonjs(), resolve()],
+  output: {
+    file: "Dart.novaextension/Scripts/main.dist.js",
+    sourcemap: true,
+    format: "cjs",
+  },
+};
