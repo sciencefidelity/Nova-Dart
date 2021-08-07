2141c4da2da2f78669f316435b21e57edb80ec7c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-07 16:33:53 +0000

    Add swatches for all material colors

--- test-workspace/lib/colors.dart
+++ test-workspace/lib/colors.dart
@@ -42,24 +42,104 @@ final Map<String, Color> colorsAccent = {
   'deepOrange': Colors.deepOrangeAccent,
 };
 
-final Map<String, Color?> colorsAmber = {
-  'amber50': Colors.amber[50],
-  'amber100': Colors.amber[100],
-  'amber200': Colors.amber[200],
-  'amber300': Colors.amber[300],
-  'amber400': Colors.amber[400],
-  'amber500': Colors.amber[500],
-  'amber600': Colors.amber[600],
-  'amber700': Colors.amber[700],
-  'amber800': Colors.amber[800],
-  'amber900': Colors.amber[900],
+final Map<String, Color?> colorsRed = {
+  'red50': Colors.red[50],
+  'red100': Colors.red[100],
+  'red200': Colors.red[200],
+  'red300': Colors.red[300],
+  'red400': Colors.red[400],
+  'red500': Colors.red[500],
+  'red600': Colors.red[600],
+  'red700': Colors.red[700],
+  'red800': Colors.red[800],
+  'red900': Colors.red[900],
 };
 
-final Map<String, Color?> colorsAmberAccent = {
-  'amberAccent100': Colors.amberAccent[100],
-  'amberAccent200': Colors.amberAccent[200],
-  'amberAccent400': Colors.amberAccent[400],
-  'amberAccent700': Colors.amberAccent[700],
+final Map<String, Color?> colorsRedAccent = {
+  'redAccent100': Colors.redAccent[100],
+  'redAccent200': Colors.redAccent[200],
+  'redAccent400': Colors.redAccent[400],
+  'redAccent700': Colors.redAccent[700],
+};
+
+final Map<String, Color?> colorsPink = {
+  'pink50': Colors.pink[50],
+  'pink100': Colors.pink[100],
+  'pink200': Colors.pink[200],
+  'pink300': Colors.pink[300],
+  'pink400': Colors.pink[400],
+  'pink500': Colors.pink[500],
+  'pink600': Colors.pink[600],
+  'pink700': Colors.pink[700],
+  'pink800': Colors.pink[800],
+  'pink900': Colors.pink[900],
+};
+
+final Map<String, Color?> colorsPinkAccent = {
+  'pinkAccent100': Colors.pinkAccent[100],
+  'pinkAccent200': Colors.pinkAccent[200],
+  'pinkAccent400': Colors.pinkAccent[400],
+  'pinkAccent700': Colors.pinkAccent[700],
+};
+
+final Map<String, Color?> colorsPurple = {
+  'purple50': Colors.purple[50],
+  'purple100': Colors.purple[100],
+  'purple200': Colors.purple[200],
+  'purple300': Colors.purple[300],
+  'purple400': Colors.purple[400],
+  'purple500': Colors.purple[500],
+  'purple600': Colors.purple[600],
+  'purple700': Colors.purple[700],
+  'purple800': Colors.purple[800],
+  'purple900': Colors.purple[900],
+};
+
+final Map<String, Color?> colorsPurpleAccent = {
+  'purpleAccent100': Colors.purpleAccent[100],
+  'purpleAccent200': Colors.purpleAccent[200],
+  'purpleAccent400': Colors.purpleAccent[400],
+  'purpleAccent700': Colors.purpleAccent[700],
+};
+
+final Map<String, Color?> colorsDeepPurple = {
+  'deepPurple50': Colors.deepPurple[50],
+  'deepPurple100': Colors.deepPurple[100],
+  'deepPurple200': Colors.deepPurple[200],
+  'deepPurple300': Colors.deepPurple[300],
+  'deepPurple400': Colors.deepPurple[400],
+  'deepPurple500': Colors.deepPurple[500],
+  'deepPurple600': Colors.deepPurple[600],
+  'deepPurple700': Colors.deepPurple[700],
+  'deepPurple800': Colors.deepPurple[800],
+  'deepPurple900': Colors.deepPurple[900],
+};
+
+final Map<String, Color?> colorsDeepPurpleAccent = {
+  'deepPurpleAccent100': Colors.deepPurpleAccent[100],
+  'deepPurpleAccent200': Colors.deepPurpleAccent[200],
+  'deepPurpleAccent400': Colors.deepPurpleAccent[400],
+  'deepPurpleAccent700': Colors.deepPurpleAccent[700],
+};
+
+final Map<String, Color?> colorsIndigo = {
+  'indigo50': Colors.indigo[50],
+  'indigo100': Colors.indigo[100],
+  'indigo200': Colors.indigo[200],
+  'indigo300': Colors.indigo[300],
+  'indigo400': Colors.indigo[400],
+  'indigo500': Colors.indigo[500],
+  'indigo600': Colors.indigo[600],
+  'indigo700': Colors.indigo[700],
+  'indigo800': Colors.indigo[800],
+  'indigo900': Colors.indigo[900],
+};
+
+final Map<String, Color?> colorsIndigoAccent = {
+  'indigoAccent100': Colors.indigoAccent[100],
+  'indigoAccent200': Colors.indigoAccent[200],
+  'indigoAccent400': Colors.indigoAccent[400],
+  'indigoAccent700': Colors.indigoAccent[700],
 };
 
 final Map<String, Color?> colorsBlue = {
@@ -80,6 +160,126 @@ final Map<String, Color?> colorsblueAccent = {
   'blueAccent200': Colors.blueAccent[200],
   'blueAccent400': Colors.blueAccent[400],
   'blueAccent700': Colors.blueAccent[700],
+};
+
+final Map<String, Color?> colorsLightBlue = {
+  'lightBlue50': Colors.lightBlue[50],
+  'lightBlue100': Colors.lightBlue[100],
+  'lightBlue200': Colors.lightBlue[200],
+  'lightBlue300': Colors.lightBlue[300],
+  'lightBlue400': Colors.lightBlue[400],
+  'lightBlue500': Colors.lightBlue[500],
+  'lightBlue600': Colors.lightBlue[600],
+  'lightBlue700': Colors.lightBlue[700],
+  'lightBlue800': Colors.lightBlue[800],
+  'lightBlue900': Colors.lightBlue[900],
+};
+
+final Map<String, Color?> colorsLightBlueAccent = {
+  'lightBlueAccent100': Colors.lightBlueAccent[100],
+  'lightBlueAccent200': Colors.lightBlueAccent[200],
+  'lightBlueAccent400': Colors.lightBlueAccent[400],
+  'lightBlueAccent700': Colors.lightBlueAccent[700],
+};
+
+final Map<String, Color?> colorsTeal = {
+  'teal50': Colors.teal[50],
+  'teal100': Colors.teal[100],
+  'teal200': Colors.teal[200],
+  'teal300': Colors.teal[300],
+  'teal400': Colors.teal[400],
+  'teal500': Colors.teal[500],
+  'teal600': Colors.teal[600],
+  'teal700': Colors.teal[700],
+  'teal800': Colors.teal[800],
+  'teal900': Colors.teal[900],
+};
+
+final Map<String, Color?> colorsTealAccent = {
+  'tealAccent100': Colors.tealAccent[100],
+  'tealAccent200': Colors.tealAccent[200],
+  'tealAccent400': Colors.tealAccent[400],
+  'tealAccent700': Colors.tealAccent[700],
+};
+
+final Map<String, Color?> colorsGreen = {
+  'green50': Colors.green[50],
+  'green100': Colors.green[100],
+  'green200': Colors.green[200],
+  'green300': Colors.green[300],
+  'green400': Colors.green[400],
+  'green500': Colors.green[500],
+  'green600': Colors.green[600],
+  'green700': Colors.green[700],
+  'green800': Colors.green[800],
+  'green900': Colors.green[900],
+};
+
+final Map<String, Color?> colorsGreenAccent = {
+  'greenAccent100': Colors.greenAccent[100],
+  'greenAccent200': Colors.greenAccent[200],
+  'greenAccent400': Colors.greenAccent[400],
+  'greenAccent700': Colors.greenAccent[700],
+};
+
+final Map<String, Color?> colorsLightGreen = {
+  'lightGreen50': Colors.lightGreen[50],
+  'lightGreen100': Colors.lightGreen[100],
+  'lightGreen200': Colors.lightGreen[200],
+  'lightGreen300': Colors.lightGreen[300],
+  'lightGreen400': Colors.lightGreen[400],
+  'lightGreen500': Colors.lightGreen[500],
+  'lightGreen600': Colors.lightGreen[600],
+  'lightGreen700': Colors.lightGreen[700],
+  'lightGreen800': Colors.lightGreen[800],
+  'lightGreen900': Colors.lightGreen[900],
+};
+
+final Map<String, Color?> colorsLightGreenAccent = {
+  'lightGreenAccent100': Colors.lightGreenAccent[100],
+  'lightGreenAccent200': Colors.lightGreenAccent[200],
+  'lightGreenAccent400': Colors.lightGreenAccent[400],
+  'lightGreenAccent700': Colors.lightGreenAccent[700],
+};
+
+final Map<String, Color?> colorsLime = {
+  'lime50': Colors.lime[50],
+  'lime100': Colors.lime[100],
+  'lime200': Colors.lime[200],
+  'lime300': Colors.lime[300],
+  'lime400': Colors.lime[400],
+  'lime500': Colors.lime[500],
+  'lime600': Colors.lime[600],
+  'lime700': Colors.lime[700],
+  'lime800': Colors.lime[800],
+  'lime900': Colors.lime[900],
+};
+
+final Map<String, Color?> colorsLimeAccent = {
+  'limeAccent100': Colors.limeAccent[100],
+  'limeAccent200': Colors.limeAccent[200],
+  'limeAccent400': Colors.limeAccent[400],
+  'limeAccent700': Colors.limeAccent[700],
+};
+
+final Map<String, Color?> colorsAmber = {
+  'amber50': Colors.amber[50],
+  'amber100': Colors.amber[100],
+  'amber200': Colors.amber[200],
+  'amber300': Colors.amber[300],
+  'amber400': Colors.amber[400],
+  'amber500': Colors.amber[500],
+  'amber600': Colors.amber[600],
+  'amber700': Colors.amber[700],
+  'amber800': Colors.amber[800],
+  'amber900': Colors.amber[900],
+};
+
+final Map<String, Color?> colorsAmberAccent = {
+  'amberAccent100': Colors.amberAccent[100],
+  'amberAccent200': Colors.amberAccent[200],
+  'amberAccent400': Colors.amberAccent[400],
+  'amberAccent700': Colors.amberAccent[700],
 };
 
 final Map<String, Color?> colorsBlueGrey = {
