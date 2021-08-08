b1464f38db742b5f06add8d8dce1fd90d2042eb1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-08 14:21:51 +0000

    Test files

---
+++ src/setSymbolTypes.ts
@@ -0,0 +1,6 @@
+import * as data from "./outline-log.json";
+
+const a = new TreeItem("name");
+
+const b = data.params.outline.children[0].element.name
+const c = data.params.outline.children[0].element.kind
