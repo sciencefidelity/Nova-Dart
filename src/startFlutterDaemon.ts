a456ae0337ad1c04594526552fdcf2ae321bdf73
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-09 21:39:02 +0000

    Try to get responses in jsonrpc

--- src/startFlutterDaemon.ts
+++ src/startFlutterDaemon.ts
@@ -1,24 +1,23 @@
 export let daemon: Process | null = null;
 
 export async function startFlutterDeamon() {
-  return new Promise((resolve, reject) => {
+  return new Promise(() => {
     if (nova.inDevMode()) {
-      const notification = new NotificationRequest("activated");
-      notification.body = "Flutter Daemon is loading";
-      nova.notifications.add(notification);
+      const daemonNotification = new NotificationRequest("daemon activated");
+      daemonNotification.body = "Flutter Daemon is loading";
+      nova.notifications.add(daemonNotification);
     }
+    console.log("Flutter Daemon is loading");
     daemon = new Process("/usr/bin/env", {
       args: ["flutter", "daemon"],
       stdio: "jsonrpc"
     });
-    const str = "";
-    daemon.onDidExit(status => {
-      if (status === 0) {
-        resolve(str);
-      } else {
-        reject(status);
-      }
+    daemon.onNotify("daemon/connected", (message) => {
+      console.log(JSON.stringify(message.result));
+      console.log(JSON.stringify(message.errorReason));
     });
     daemon.start();
+  }).then(function() {
+    console.log("Daemon started");
   });
 }
