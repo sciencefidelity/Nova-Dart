fafec82b8f7a49166058a8a99eb4d1413dc6e937
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-18 00:01:12 +0000

    Set up logs for the flutter daemon

---
+++ Dart.novaextension/daemon.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd "$WORKSPACE_DIR"
+
+flutter \
+daemon
--- src/flutterDaemonService.ts
+++ src/flutterDaemonService.ts
@@ -1,4 +1,6 @@
+import { cleanPath } from "nova-extension-utils"
 import { state } from "./globalVars"
+import { makeFileExecutable } from "./utils/utils"
 
 export class FlutterDaemonService {
   process: Process | null
@@ -10,16 +12,36 @@ export class FlutterDaemonService {
     this.stop = this.stop.bind(this)
   }
 
-  start() {
+  async start() {
+    console.log("Loading Flutter Daemon")
+    let _args = ["flutter", "daemon"]
+    let _env = {}
     if (nova.inDevMode()) {
       const daemonNotification = new NotificationRequest("daemon activated")
       daemonNotification.body = "Flutter Daemon is loading"
       nova.notifications.add(daemonNotification)
+      // create logs in dev mode
+      const daemonFile = nova.path.join(nova.extension.path, "daemon.sh")
+      await makeFileExecutable(daemonFile)
+      const logDir = nova.path.join(nova.workspace.path!, "logs")
+      await new Promise<void>((resolve, reject) => {
+        const p = new Process("/usr/bin/env", {
+          args: ["mkdir", "-p", logDir]
+        })
+        p.onDidExit((status) => (status === 0 ? resolve() : reject()))
+        p.start()
+      })
+      console.log("logging to", logDir)
+      const outLog = nova.path.join(logDir, "app.log")
+      _args = ["bash", "-c", `"${daemonFile}" | tee "${outLog}"`]
+      _env = {
+        WORKSPACE_DIR: `${cleanPath(nova.workspace.path!)}/test-workspace` ?? "",
+      }
     }
-    console.log("Loading Flutter Daemon")
 
     const daemon = new Process("/usr/bin/env", {
-      args: ["flutter", "daemon"],
+      args: _args,
+      env: _env,
       stdio: "jsonrpc"
     })
 
