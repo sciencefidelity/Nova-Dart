7261d1101bf79616e0a683a472fcc5a689a6fd3f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-19 23:20:22 +0000

    Organise imports

--- src/flutterDaemonService.ts
+++ src/flutterDaemonService.ts
@@ -1,7 +1,6 @@
 import { cleanPath } from "nova-extension-utils"
 import { keys, state, vars } from "./globalVars"
 import { makeFileExecutable, wrapCommand } from "./utils/utils"
-// import { Message } from "./interfaces/interfaces"
 
 export class FlutterDaemonService {
   process: Process | null
