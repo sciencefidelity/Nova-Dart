cf2d37ad25eaf94b8f935a0bde26d6467d4f4f53
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-19 23:25:06 +0000

    Move all utils to one file

--- src/utils/novaUtils.ts
+++
@@ -1,7 +0,0 @@
-export async function openFile(uri: string) {
-  const newEditor = await nova.workspace.openFile(uri)
-  if (newEditor) return newEditor
-  console.warn("Failed to open, retrying once", uri)
-  // try one more time, this doesn't resolve if the file isn't already open
-  return await nova.workspace.openFile(uri)
-}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -27,6 +27,14 @@ export async function makeFileExecutable(file: string) {
   })
 }
 
+export async function openFile(uri: string) {
+  const newEditor = await nova.workspace.openFile(uri)
+  if (newEditor) return newEditor
+  console.warn("Failed to open, retrying once", uri)
+  // try one more time, this doesn't resolve if the file isn't already open
+  return await nova.workspace.openFile(uri)
+}
+
 export const showActionableError = (
   id: string,
   title: string,
